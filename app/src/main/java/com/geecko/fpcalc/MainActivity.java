6a4fca963d06d2d07372b6a8c6b055d980e735f6
steve <svenoaks@gmail.com>
2018-02-08 18:29:47 -0500

    building libfpcalc.so

--- app/src/main/java/com/geecko/fpcalc/MainActivity.java
+++ app/src/main/java/com/geecko/fpcalc/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends Activity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("fpcalc");
     }
 
     @Override
@@ -18,12 +18,12 @@ public class MainActivity extends Activity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+
 }
