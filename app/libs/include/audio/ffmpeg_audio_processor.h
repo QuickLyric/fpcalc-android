46dd419f87e8eb1adb39cd2dfcdae85719ede0dc
steve <svenoaks@gmail.com>
2018-02-08 07:38:14 -0500

    initial

---
+++ app/libs/include/audio/ffmpeg_audio_processor.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2016  Lukas Lalinsky
+// Distributed under the MIT license, see the LICENSE file for details.
+
+#ifndef CHROMAPRINT_AUDIO_FFMPEG_AUDIO_PROCESSOR_H_
+#define CHROMAPRINT_AUDIO_FFMPEG_AUDIO_PROCESSOR_H_
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "audio/ffmpeg_audio_processor_swresample.h"
+
+#endif
